107cde9d2466fc2a8e3a12ad13cfdab0b5bd10f0
AlexisLuna <alexisgabriel.lu@gmail.com>
2024-06-13 10:51:22 -0500

    Add pag escultura, favicon, fix menus

--- main.js
+++ main.js
@@ -20,3 +20,20 @@ var myModal = new bootstrap.Modal(document.getElementById('miModal'), {
     backdrop: 'static', // Esto asegura que el fondo sea oscuro y estático
     keyboard: false
 });
+// Selecciona todos los elementos con la clase 'item_sur' y agrega un evento de clic a cada uno
+document.querySelectorAll('.item_sur').forEach(function(item) {
+    item.addEventListener('click', function() {
+        // Captura los elementos del card específico que fue clickeado
+        var titulo = item.querySelector('.TituloModal').textContent;
+        var imagen = item.querySelector('img').src;
+        var descripcion = item.querySelector('.Descrip').textContent;
+
+        // Asigna los valores capturados a la modal
+        document.getElementById('tituloModal').textContent = titulo;
+        document.querySelector('.modal-body img').src = imagen;
+        document.querySelector('.modal-body p').textContent = descripcion;
+
+        // Muestra la modal
+        myModal.show();
+    });
+})
